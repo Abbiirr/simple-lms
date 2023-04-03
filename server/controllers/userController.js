b159eb649b8a4859259e2af9ebbed4259e84395c
Abir <abirhossain9@iut-dhaka.edu>
2023-04-03 15:11:00 +0600

    Adds user delete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import { db } from "../config/db.js";
+import jwt from "jsonwebtoken";
 
 const getUsers = async (req, res, next) => {
   try {
@@ -20,4 +21,46 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-export default { getUsers };
+const deleteUser = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decode = jwt.verify(token, "SECRET");
+
+    const id = decode.id;
+
+    const [rows, fields] = await db
+      .promise()
+      .query(`SELECT * FROM users WHERE id = ?`, [id]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+      });
+    }
+
+    const user = rows[0];
+
+    if (decode.role !== "Librarian") {
+      return res.status(403).json({
+        status: false,
+        message: "Access denied",
+      });
+    }
+
+    await db.promise().query(`DELETE FROM users WHERE id = ?`, [id]);
+
+    res.status(200).json({
+      status: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: false,
+      message: "Server error",
+    });
+  }
+};
+
+export default { getUsers, deleteUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,5 +3,6 @@ import userController from "../controllers/userController.js";
 const router = express.Router();
 
 router.get("/", userController.getUsers);
+router.delete("/:id", userController.deleteUser);
 
 export default router;
