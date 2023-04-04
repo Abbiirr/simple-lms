60f6ce0e6a55e55fc05d484beb29af23f8153bd9
Abir <abirhossain9@iut-dhaka.edu>
2023-04-04 16:09:56 +0600

    Fixes book display

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -3,16 +3,26 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
 // import "./templatesscreen.css";
+import jwt_decode from "jwt-decode";
 
 const OrderScreen = () => {
   const [books, setBooks] = useState([]);
+  const token = localStorage.getItem("token");
+
+  const checkIfLibrarian = (token) => {
+    if (!token) return false;
+    const decode = jwt_decode(token);
+    const role = decode.role;
+    if (role === "Librarian") return true;
+    return false;
+  };
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
         const response = await axios.get("/api/books");
         console.log(response.data);
-        setBooks(response.data);
+        setBooks(response.data.books);
       } catch (error) {
         console.error(error);
       }
@@ -23,43 +33,59 @@ const OrderScreen = () => {
 
   return (
     <div className="container">
-      {/* {books.length === 0 ? (
-        <div className="loader"><h4>Loading books...</h4></div> 
-      ) : ( */}
-      <div className="row">
-        <div className="temp-header">
-          <h1 className="my-3">Books</h1>
+      {books.length === 0 ? (
+        <div className="loader">
+          <h4>Loading books...</h4>
         </div>
-        {books.map((book) => (
-          <div key={book._id} className="body-style">
-            <Card className="style-card">
-              {/* <Card.Img variant="top" src={book.thumbnail} /> */}
-              <Card.Body>
-                {/* <Card.Title>{book._id}</Card.Title> */}
-                <Card.Text>{book.name}</Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>
-                  <Link to={`/viewbook/${book._id}`}>
-                    View Book: {book.name}
-                  </Link>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <Link to={`/editbook/${book._id}`}>
-                    Edit Book: {book.name}
-                  </Link>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <Link to={`/borrowbook/${book._id}`}>
-                    Borrow Book: {book.name}
-                  </Link>
-                </ListGroupItem>
-              </ListGroup>
-            </Card>
+      ) : (
+        <div className="row">
+          <div className="temp-header">
+            <h1 className="my-3">Books</h1>
           </div>
-        ))}
-      </div>
-      {/* )} */}
+          {books.map((book) => (
+            <div key={book._id} className="col-lg-4 col-md-6 mb-4">
+              <Card className="h-100">
+                {/* <Card.Img variant="top" src={book.thumbnail} /> */}
+                <Card.Body>
+                  {/* <Card.Title>{book._id}</Card.Title> */}
+                  <Card.Text>{book.name}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Author: {book.author}</ListGroupItem>
+                  <ListGroupItem>Genre: {book.genre}</ListGroupItem>
+                  <ListGroupItem>Status: {book.status}</ListGroupItem>
+                  <ListGroupItem>
+                    Description: {book.short_description}
+                  </ListGroupItem>
+                </ListGroup>
+                <Card.Footer className="text-center">
+                  <Link
+                    to={`/viewbook/${book._id}`}
+                    className="btn btn-primary mx-2"
+                  >
+                    View Book
+                  </Link>
+
+                  <Link
+                    to={`/borrowbook/${book._id}`}
+                    className="btn btn-success mx-2"
+                  >
+                    Borrow Book
+                  </Link>
+                  {checkIfLibrarian(token) ? (
+                    <Link
+                      to={`/editbook/${book._id}`}
+                      className="btn btn-secondary mx-2"
+                    >
+                      Edit Book
+                    </Link>
+                  ) : null}
+                </Card.Footer>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
